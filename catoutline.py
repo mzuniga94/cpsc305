f63827b031cc0865de8818eb246b681176889122
Matthew Zuniga <mzuniga94@csu.fullerton.edu>
2018-09-26 20:24:39 -0700

    Adding comments

--- catoutline.py
+++ catoutline.py
@@ -1,3 +1,5 @@
+# We are creating an outline of a cat using turtle library.
+
 import turtle
 
 turtle.setup(800, 800)
@@ -5,9 +7,7 @@ wn = turtle.Screen()
 
 thomas = turtle.Turtle()
 
-#no color yet bc outline is black
-
-#head
+# drawing head
 thomas.circle(150)
 thomas.penup()
 thomas.left(90)
@@ -16,7 +16,7 @@ thomas.left(90)
 thomas.forward(111)
 thomas.right(90)
 
-#left ear
+# drawing left ear
 thomas.pendown()
 thomas.forward(110)
 thomas.right(130)
@@ -26,7 +26,7 @@ thomas.left(40)
 thomas.penup()
 thomas.forward(60)
 
-#right ear
+# drawing right ear
 thomas.pendown()
 thomas.left(40)
 thomas.forward(110)
@@ -37,7 +37,7 @@ thomas.penup()
 thomas.forward(120)
 thomas.left(100)
 
-#whiskers
+# drawing whiskers
 thomas.pendown()
 thomas.forward(150)
 thomas.penup()
@@ -47,7 +47,7 @@ thomas.pendown()
 thomas.right(90)
 thomas.forward(140)
 
-#other whiskers
+# drawing other whiskers
 thomas.penup()
 thomas.left(10)
 thomas.forward(220)
@@ -61,21 +61,21 @@ thomas.pendown()
 thomas.right(90)
 thomas.forward(140)
 
-#left eye
+# drawing left eye
 thomas.penup()
 thomas.setheading(45)
 thomas.forward(80)
 thomas.pendown()
 thomas.circle(25)
 
-#right eye
+# drawing right eye
 thomas.penup()
-thomas.setheading(0) # sets the direction of the turtle object
+thomas.setheading(0)
 thomas.forward(90)
 thomas.pendown()
 thomas.circle(25)
 
-#nose
+# drawing nose
 thomas.penup()
 thomas.setheading(210)
 thomas.forward(60)
@@ -92,7 +92,7 @@ thomas.forward(10)
 thomas.setheading(270)
 thomas.forward(10)
 
-#mouth
+# drawing mouth
 thomas.pendown()
 thomas.forward(25)
 thomas.setheading(210)
