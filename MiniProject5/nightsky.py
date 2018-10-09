e0140c42e28805087aca632f47c390d5389b69b1
Matthew Zuniga <mzuniga94@csu.fullerton.edu>
2018-10-09 10:42:09 -0700

    Merged stuff

--- MiniProject5/nightsky.py
+++ MiniProject5/nightsky.py
@@ -9,30 +9,21 @@ def draw_sky(turt):
 
         # choose random x and y coordinates
         # TODO: you need to rewrite these statements to generate coordinates
-        # over the entire window, not just near the origin
+        # over the entire window, not just near the origins
         x = random.randrange(-400, 400)
         y = random.randrange(-400, 400)
 
         # choose random star type; first generate an integer between
         # 0 and 99 inclusive
-<<<<<<< HEAD
-        percent = random.randrange(0, 100)
+
+        percent = random.randrange(0, 99)
         # TODO: now use if statement(s) on percent, to decide whether to
         if percent <= 40:
             blue_star(turt, x, y)
-        elif percent >= 55:
+        elif percent >= 55 and percent <= 75:
             red_star(turt, x, y)
-        else:
-            big_star(turt, x, y)
-=======
-        percent = random.randrange(0, 99)
-        # TODO: now use if statement(s) on percent, to decide whether to
-        if percent <= 75:
-            blue_star(turt, x, y)
         elif percent >= 75:
-            draw_embellishment(turt, x, y)
->>>>>>> 606e501a74dee93bf03925d22de58c9fe93cc532
-        # call blue_star, red_star, or big_star
+            big_star(turt, x, y)
 
 # Draw a small blue star at x, y, using turt.
 def blue_star(turt, x, y):
@@ -48,20 +39,9 @@ def blue_star(turt, x, y):
 def red_star(turt, x, y):
     turt.penup()
     turt.goto(x, y)
-<<<<<<< HEAD
     turt.pendown()
     turt.color(1, random.uniform(0.67, 1), random.uniform(0.70, 1))
     turt.circle(random.uniform(0.5, 3))
-
-=======
-    turt.penup()
-    turt.goto(x, y)
-    turt.pendown()
-    turt.color(0, 0, random.uniform(0.5, 1)) #TODO: Please change this to red!
-    turt.circle(5)
->>>>>>> 606e501a74dee93bf03925d22de58c9fe93cc532
-    # TODO: add statements to pick a random red-white color, and random
-    # radius, and then draw a dot using that color and radius
 
 # Draw a large four-pointed star at x, y using turt.
 def big_star(turt, x, y):
