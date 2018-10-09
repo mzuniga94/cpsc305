7c0c2a2c6d465bfd2141159fa3d0e39113619a05
olipatane <opatane@csu.fullerton.edu>
2018-10-09 12:01:45 -0700

    big stars and percent change

--- MiniProject5/nightsky.py
+++ MiniProject5/nightsky.py
@@ -20,9 +20,9 @@ def draw_sky(turt):
         # TODO: now use if statement(s) on percent, to decide whether to
         if percent <= 40:
             blue_star(turt, x, y)
-        elif percent >= 55 and percent <= 75:
+        elif percent >= 55 and percent <= 88:
             red_star(turt, x, y)
-        elif percent >= 75:
+        elif percent >= 88:
             big_star(turt, x, y)
 
 # Draw a small blue star at x, y, using turt.
@@ -31,27 +31,49 @@ def blue_star(turt, x, y):
     turt.goto(x, y)
     turt.pendown()
     turt.color(random.uniform(0.71, 1), random.uniform(0.87, 1), 1)
-    turt.circle(random.uniform(0.5, 3))
-    # TODO: add statements to pick a random blue-white color, and random
-    # radius, and then draw a dot using that color and radius
-
+    turt.begin_fill()
+    turt.circle(random.uniform(0.1, 2))
+    turt.end_fill()
+       
 # Draw a medium-sized red star at x, y using turt.
 def red_star(turt, x, y):
     turt.penup()
     turt.goto(x, y)
     turt.pendown()
     turt.color(1, random.uniform(0.67, 1), random.uniform(0.70, 1))
-    turt.circle(random.uniform(0.5, 3))
+    turt.begin_fill()
+    turt.circle(random.uniform(1.5, 3.5))
+    turt.end_fill()
 
 # Draw a large four-pointed star at x, y using turt.
 def big_star(turt, x, y):
-    turt.goto(x, y)
     turt.penup()
     turt.goto(x, y)
     turt.pendown()
-    turt.color(0, 0, random.uniform(0.5, 1)) #TODO: Please change this to white color!
-    turt.circle(5)
-    # TODO: add statements to draw a four-pointed white star
+    turt.color(1, 1, 1)
+    turt.begin_fill()
+    turt.forward(20)
+    turt.left(160)
+    turt.forward(20)
+    turt.left(20)
+    turt.forward(20)
+    turt.left(160)
+    turt.forward(20)
+    turt.left(100)
+    turt.end_fill()
+    turt.begin_fill()
+    turt.forward(3.47)
+    turt.right(90)
+    turt.forward(3.7)
+    turt.left(120)
+    turt.forward(15)
+    turt.left(150)
+    turt.forward(15)
+    turt.left(30)
+    turt.forward(15)
+    turt.left(150)
+    turt.forward(15)
+    turt.end_fill()
 
 # Draw some kind of swirly
 def draw_embellishment(turt, x, y):
